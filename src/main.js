175a9a70dabdf16bd9842aed1be7ed43d874b305
wangxiaowei520 <WaNgWiaOWei>
2019-12-19 15:32:56 +0800

    黑马头条PC-初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './styles/index.less'// 引入初始化样式
+// import ElementUI from 'element-ui'
 Vue.config.productionTip = false
 
 new Vue({
